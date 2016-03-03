4dfa4d08531e26eeed48f20f570c7c8b0bacc7fb
jenkins_training <chef.opscode.training@gmail.com>
2016-03-03 19:21:19 +0530

    my first git code commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
+
